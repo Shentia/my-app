466c69acb232ccdc312bdba6e3973ddf58689bef
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2025-08-14 09:25:04 -0400

    initialize Angular application with basic structure and components

---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Product} from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductList {
+  products: Product[] = [
+    {id:1, title: 'Keyboard'},
+    {id:2, title: 'Microphone'},
+    {id:3, title: 'Web camera'},
+    {id:4, title: 'Tablet'},
+  ]
+}
